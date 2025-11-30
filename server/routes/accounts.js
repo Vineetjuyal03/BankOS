446e61c19e683573dc99b65ab00a2a32ea693709
Vineetjuyal03 <vineetjuyal147@gmail.com>
2025-11-30 09:27:59 +0530

    fix2 for deployment

--- server/routes/accounts.js
+++ server/routes/accounts.js
@@ -12,7 +12,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false }
+  ssl: {
+    minVersion: 'TLSv1.2',
+    rejectUnauthorized: true
+  }
 });
 
 // Middleware to verify JWT and get userId
